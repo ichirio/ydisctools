f800c3834895154504bb81d9a29245d94eb06d1d
ichirio <masui.yoichi@gmail.com>
2024-09-16 15:29:11 +0900

    V0.0.1 alpha1 release

---
+++ R/compute_dy.R
@@ -0,0 +1,17 @@
+#' Compute Day Difference Between Two Dates
+#'
+#' This function calculates the day difference between a reference date and an event date. If the difference is non-negative, the result is incremented by 1 day.
+#'
+#' @param refdate A reference date, typically the starting point. Can be a character string or Date object.
+#' @param eventdate An event date, typically the comparison date. Can be a character string or Date object.
+#' @return The difference in days between the reference date and the event date. If the difference is non-negative, the result is incremented by 1.
+#' @examples
+#' compute_dy("2023-09-15", "2023-09-20")
+#' @export
+#' @importFrom lubridate interval as.Date
+#' @importFrom dplyr if_else
+compute_dy <- function(refdate, eventdate) {
+  interval(as.Date(refdate), as.Date(eventdate)) %>%
+    as.numeric(units = "days") %>%
+    if_else(. >= 0, . + 1, .)
+}
