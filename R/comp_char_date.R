eb307c85483603800576038e1c4ef10c08700f36
ichirio <masui.yoichi@gmail.com>
2025-02-13 12:27:19 +0900

    V0.0.56 alpha release

--- R/comp_char_date.R
+++ R/comp_char_date.R
@@ -6,6 +6,11 @@
 #' @param y A character string representing the second date.
 #' @param mark A character string representing the comparison operator.
 #'             Must be one of "<", "<=", ">", ">=", "==", "!=". Default is "<=".
+#' @param mode A character string representing the comparison mode.
+#'             Must be one of "S", "IMAX", "IMIN". Default is "<=".
+#'             S: Cmpare the dates with shorter length.
+#'             IMAX: Cmpare the dates imputing as max value. e.g.) 2025-01 can be imputed as 2025-01-99
+#'             IMIN: Cmpare the dates imputing as min value. e.g.) 2025-01 can be imputed as 2025-01-00
 #' @return A logical value indicating the result of the comparison, or NA if the input is invalid.
 #' @examples
 #' comp_char_date("2023-10-01", "2023-10-02", "<=") # TRUE
@@ -14,16 +19,29 @@
 #'
 #' @import stringr
 #' @export
-comp_char_date <- function(x, y, mark = "<=") {
+comp_char_date <- function(x, y, mark = "<=", mode = "S") {
   if(is.null(x) | is.null(y)) return(NA)
   if(is.na(x) | is.na(y)) return(NA)
   if(!(mark %in% c("<", "<=", ">", ">=", "==", "!="))) return(NA)
 
-  len <- min(str_length(x), str_length(y))
-  if(len < 4) return(NA)
-
-  x_sub <- str_sub(x, 1, len)
-  y_sub <- str_sub(y, 1, len)
+  if(mode %in% c("IMAX", "IMIN")) {
+    add_char <- if(mode == "IMAX") "~" else "\x09"
+    if(str_length(x) > str_length(y)) {
+      x_sub <- x
+      y_sub <- paste0(x, add_char)
+    }else if(str_length(x) < str_length(y)) {
+      x_sub <- paste0(x, add_char)
+      y_sub <- y
+    }else {
+      x_sub <- x
+      y_sub <- y
+    }
+  } else {
+    len <- min(str_length(x), str_length(y))
+    if(len < 4) return(NA)
+    x_sub <- str_sub(x, 1, len)
+    y_sub <- str_sub(y, 1, len)
+  }
 
   return(eval(parse(text = paste("x_sub", mark, "y_sub"))))
 }
@@ -37,6 +55,11 @@ comp_char_date <- function(x, y, mark = "<=") {
 #' @param y A character vector representing the second set of dates.
 #' @param mark A character string representing the comparison operator.
 #'             Must be one of "<", "<=", ">", ">=", "==", "!=". Default is "<=".
+#' @param mode A character string representing the comparison mode.
+#'             Must be one of "S", "IMAX", "IMIN". Default is "<=".
+#'             S: Cmpare the dates with shorter length.
+#'             IMAX: Cmpare the dates imputing as max value. e.g.) 2025-01 can be imputed as 2025-01-99
+#'             IMIN: Cmpare the dates imputing as min value. e.g.) 2025-01 can be imputed as 2025-01-00
 #' @return A logical vector indicating the result of the comparison for each element, or an error if the input vectors have different lengths.
 #' @examples
 #' x <- c("2023-10-01", "2023-10-02")
@@ -46,8 +69,8 @@ comp_char_date <- function(x, y, mark = "<=") {
 #'
 #' @import purrr
 #' @export
-comp_char_dates <- function(x, y, mark = "<=") {
+comp_char_dates <- function(x, y, mark = "<=", mode = "S") {
   if(length(x) != length(y)) stop("The input vectors must have the same length.")
 
-  return(map_lgl(seq_along(x), ~ comp_char_date(x[.x], y[.x], mark)))
+  return(map_lgl(seq_along(x), ~ comp_char_date(x[.x], y[.x], mark, mode)))
 }
