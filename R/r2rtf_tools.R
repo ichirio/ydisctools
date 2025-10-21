9f31829366b39e8a765255231f39425a13bb9302
ichirio <masui.yoichi@gmail.com>
2025-10-21 11:07:57 +0900

    v0.0.225 alpha release

--- R/r2rtf_tools.R
+++ R/r2rtf_tools.R
@@ -1430,7 +1430,8 @@ assemble_rtf <- function(input,
 
     bookmark_rtf <- paste0(
       "\\ltrpar\\s2\\ql {\\fs0 \\cf", white_num, " ", title, "} \\par \\s0\\ql ",
-      "{\\*\\bkmkstart ", bookmark_keys, "} \\par \\ql {\\*\\bkmkend ", bookmark_keys, "}\n"
+      # "{\\*\\bkmkstart ", bookmark_keys, "} \\par \\ql {\\*\\bkmkend ", bookmark_keys, "}\n"
+      "{\\*\\bkmkstart ", bookmark_keys, "}{\\*\\bkmkend ", bookmark_keys, "}\n"
     )
   }
 
